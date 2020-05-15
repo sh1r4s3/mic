4831a4b47cc88826463f780137e8c4f71ab92de1
Nikita Ermakov <coffe92@gmail.com>
2020-05-15 21:47:33 +0300

    Add register abstraction

---
+++ src/register.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+#include "misc.h"
+
+struct reg
+{
+  char *data;
+  const char *name;
+  int bits;
+};
+
+struct reg *reg_create(int bits, const char *name);
+void reg_free(struct reg *r);
