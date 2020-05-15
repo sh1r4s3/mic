1c2ec61375d22b1c1e42d3e34a1dea9e837eb590
Nikita Ermakov <coffe92@gmail.com>
2020-05-15 21:48:30 +0300

    Add shift register abstraction

---
+++ src/shift.c
@@ -0,0 +1,34 @@
+#include "shift.h"
+
+void shift_sll8(char *line, int size)
+{
+  if (!line)
+    return;
+ 
+  if (size > 8)
+  {
+    for (int i = size - 9; i >= 0; --i)
+    {
+      line[i + 8] = line[i];
+      line[i] = 0;
+    }
+  }
+  else
+  {
+    for (int i = 0; i < size; ++i)
+    {
+      line[i] = 0;
+    }
+  }
+}
+
+void shift_sra1(char *line, int size)
+{
+  if (!line)
+    return;
+
+  for (int i = 0; i < size - 1; ++i)
+  {
+    line[i] = line[i + 1];
+  }
+}
---
+++ src/shift.h
@@ -0,0 +1,2 @@
+void shift_sll8(char *line, int size);
+void shift_sra1(char *line, int size);
