9a43ddf80902fb7c8482fb44fc1e11864b1e5ab2
Nikita Ermakov <coffe92@gmail.com>
2020-05-18 23:04:31 +0300

    microcode: Add microcode loading from a file

--- src/microcode.c
+++ src/microcode.c
@@ -19,6 +19,29 @@ void microcode_free(struct microcode *mcode)
   free(mcode);
 }
 
+struct microcode *microcode_load(const char *fname)
+{
+  struct stat s;
+  int fd = open(fname, O_RDONLY);
+  if (fd < 0)
+    ERR("Can't open file %s / errno=%d / %s", fname, errno, strerror(errno));
+
+  if (fstat(fd, &s) < 0)
+    ERR("Can't get size of the file %s / errno=%d / %s", fname, errno, strerror(errno));
+
+  // Basic check
+  if (s.st_size % sizeof(struct mir))
+    ERR("It is seems that the file is corrupted");
+
+  int cmds = s.st_size/sizeof(struct mir);
+  struct microcode *mcode = microcode_create(cmds);
+  ssize_t sz = read(fd, mcode, sizeof(struct mir)*cmds);
+  if (sz != sizeof(struct mir)*cmds)
+    ERR("Can't read microcode from the file (read %li)", sz);
+
+  close(fd);
+}
+
 struct mir *process_cmd(struct microcode *mcode, struct mir *cmd)
 {
   return mcode->cmd + cmd->next_address/sizeof(struct mir);
--- src/microcode.h
+++ src/microcode.h
@@ -1,5 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 #include "misc.h"
 #include "mir.h"
 
@@ -11,4 +16,5 @@ struct microcode
 
 struct microcode *microcode_create(int n_cmds);
 void microcode_free(struct microcode *mcode);
+struct microcode *microcode_load(const char *fname);
 struct mir *process_cmd(struct microcode *mcode, struct mir *cmd);
